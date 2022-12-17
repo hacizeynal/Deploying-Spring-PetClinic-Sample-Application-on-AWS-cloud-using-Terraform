1c3bcabeb7c1282ee8ffb6a8cb9c998ac7b8f24d
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-12-18 00:13:19 +0100

    add health check script to app.sh

--- application.sh
+++ application.sh
@@ -9,4 +9,5 @@ cd home/ubuntu
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
 sudo sed -i "s/localhost/10.20.15.200:3306/g" src/main/resources/application-mysql.properties
+sudo sh -c "echo 'management.endpoint.health.group.custom.include=diskSpace,ping' >> src/main/resources/application.properties"
 mvn spring-boot:run -Dspring-boot.run.profiles=mysql
