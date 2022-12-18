dc8be7ee08102183f154ff8d9e1300342e944b44
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-12-18 23:11:14 +0100

    change S3 bucket on AWS for tfstate

--- application.sh
+++ application.sh
@@ -9,5 +9,10 @@ cd home/ubuntu
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
 sudo sed -i "s/localhost/10.20.15.200:3306/g" src/main/resources/application-mysql.properties
-sudo sh -c "echo 'management.endpoint.health.group.custom.include=diskSpace,ping' >> src/main/resources/application.properties"
+# sudo sh -c "echo 'management.endpoint.health.group.custom.include=diskSpace,ping' >> src/main/resources/application.properties"
+sudo sh -c "echo 'management.endpoint.health.group.custom.status.http-mapping.up=207' >> src/main/resources/application.properties"
 mvn spring-boot:run -Dspring-boot.run.profiles=mysql
+
+
+
+management.endpoint.health.group.custom.status.http-mapping.up=207
