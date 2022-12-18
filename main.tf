1c17f3aa2ac4705bc6765663711c65a334dd2e3e
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-12-19 00:19:36 +0100

    change status code for health group

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-zhajili"
-    key    = "terraform-state/petclinic/terraform.tfstate"
+    key    = "petclinic/terraform.tfstate"
     region = "us-east-1"
   }
 }
